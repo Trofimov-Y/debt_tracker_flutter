88294c3bbabd8ea37b5b9f4c09e4ccc008f886b1
Yuriy Trofimov <yuriy.trofimov.ua@gmail.com>
2023-06-07 00:50:19 +0300

    Add useful text style extensions

---
+++ lib/extensions/text_style_extensions.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/material.dart';
+
+extension TextStyleExtensions on TextStyle {
+  TextStyle get bold => copyWith(fontWeight: FontWeight.w700);
+
+  TextStyle get semiBold => copyWith(fontWeight: FontWeight.w600);
+
+  TextStyle get medium => copyWith(fontWeight: FontWeight.w500);
+
+  TextStyle get regular => copyWith(fontWeight: FontWeight.w400);
+
+  TextStyle withColor(Color color) => copyWith(color: color);
+}
