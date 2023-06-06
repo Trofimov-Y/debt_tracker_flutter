6063c4079574782f0c31f76ea603d837791956e3
Yuriy Trofimov <yuriy.trofimov.ua@gmail.com>
2023-06-07 00:49:39 +0300

    Add text theme config

---
+++ lib/theme/text_theme.dart
@@ -0,0 +1,21 @@
+import 'package:flutter/material.dart';
+
+const fontFamily = 'Poppins';
+
+const textTheme = TextTheme(
+  displayLarge: TextStyle(fontSize: 57, height: 1.12, fontWeight: FontWeight.normal),
+  displayMedium: TextStyle(fontSize: 45, height: 1.16, fontWeight: FontWeight.normal),
+  displaySmall: TextStyle(fontSize: 36, height: 1.22, fontWeight: FontWeight.normal),
+  headlineLarge: TextStyle(fontSize: 32, height: 1.25, fontWeight: FontWeight.normal),
+  headlineMedium: TextStyle(fontSize: 28, height: 1.29, fontWeight: FontWeight.normal),
+  headlineSmall: TextStyle(fontSize: 24, height: 1.33, fontWeight: FontWeight.normal),
+  titleLarge: TextStyle(fontSize: 22, height: 1.27, fontWeight: FontWeight.normal),
+  titleMedium: TextStyle(fontSize: 16, height: 1.5, fontWeight: FontWeight.w500),
+  titleSmall: TextStyle(fontSize: 16, height: 1.43, fontWeight: FontWeight.w500),
+  bodyLarge: TextStyle(fontSize: 16, height: 1.5, fontWeight: FontWeight.normal),
+  bodyMedium: TextStyle(fontSize: 14, height: 1.43, fontWeight: FontWeight.normal),
+  bodySmall: TextStyle(fontSize: 12, height: 1.33, fontWeight: FontWeight.normal),
+  labelLarge: TextStyle(fontSize: 14, height: 1.43, fontWeight: FontWeight.w500),
+  labelMedium: TextStyle(fontSize: 12, height: 1.33, fontWeight: FontWeight.w500),
+  labelSmall: TextStyle(fontSize: 11, height: 1.45, fontWeight: FontWeight.w500),
+);
