13beba48e8752631657dc6faf4d38c21ba903368
Yuriy Trofimov <yuriy.trofimov.ua@gmail.com>
2023-06-08 21:41:06 +0300

    Add sign_in page route

--- lib/presentation/pages/start/start_page.dart
+++ lib/presentation/pages/start/start_page.dart
@@ -1,5 +1,5 @@
 import 'package:auto_route/auto_route.dart';
-import 'package:debt_tracker/presentation/cubit/cubit/authentication_cubit.dart';
+import 'package:debt_tracker/presentation/pages/start/cubits/authentication/authentication_cubit.dart';
 import 'package:debt_tracker/presentation/routing/app_router.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
