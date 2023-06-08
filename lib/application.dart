f745d12287c668d2753c5b612025266a0bd1b51d
Yuriy Trofimov <yuriy.trofimov.ua@gmail.com>
2023-06-08 23:09:06 +0300

    Wrap Material app with KeyboardDismissOnTap

--- lib/application.dart
+++ lib/application.dart
@@ -2,6 +2,7 @@ import 'package:debt_tracker/presentation/routing/app_router.dart';
 import 'package:debt_tracker/presentation/routing/app_router_observer.dart';
 import 'package:debt_tracker/presentation/theme/themes.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_keyboard_visibility/flutter_keyboard_visibility.dart';
 import 'package:get_it/get_it.dart';
 
 class Application extends StatelessWidget {
@@ -9,14 +10,16 @@ class Application extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp.router(
-      debugShowCheckedModeBanner: false,
-      theme: theme,
-      darkTheme: darkTheme,
-      routerConfig: GetIt.instance.get<AppRouter>().config(
-        navigatorObservers: () {
-          return [GetIt.instance.get<AppRouterObserver>()];
-        },
+    return KeyboardDismissOnTap(
+      child: MaterialApp.router(
+        debugShowCheckedModeBanner: false,
+        theme: theme,
+        darkTheme: darkTheme,
+        routerConfig: GetIt.instance.get<AppRouter>().config(
+          navigatorObservers: () {
+            return [GetIt.instance.get<AppRouterObserver>()];
+          },
+        ),
       ),
     );
   }
