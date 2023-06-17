12693ac8d6f2a87197b8dba30c6da83e06ec3baa
Yuriy Trofimov <yuriy.trofimov.ua@gmail.com>
2023-06-17 21:37:32 +0300

    Add localization config to material app

--- lib/application.dart
+++ lib/application.dart
@@ -1,9 +1,11 @@
+import 'package:debt_tracker/generated/l10n.dart';
 import 'package:debt_tracker/presentation/routing/app_router.dart';
 import 'package:debt_tracker/presentation/routing/app_router_observer.dart';
 import 'package:debt_tracker/presentation/theme/themes.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_keyboard_visibility/flutter_keyboard_visibility.dart';
 import 'package:get_it/get_it.dart';
+import 'package:flutter_localizations/flutter_localizations.dart';
 
 class Application extends StatelessWidget {
   const Application({super.key});
@@ -13,6 +15,13 @@ class Application extends StatelessWidget {
     return KeyboardDismissOnTap(
       child: MaterialApp.router(
         debugShowCheckedModeBanner: false,
+        localizationsDelegates: const [
+          S.delegate,
+          GlobalMaterialLocalizations.delegate,
+          GlobalWidgetsLocalizations.delegate,
+          GlobalCupertinoLocalizations.delegate,
+        ],
+        supportedLocales: S.delegate.supportedLocales,
         theme: theme,
         darkTheme: darkTheme,
         routerConfig: GetIt.instance.get<AppRouter>().config(
