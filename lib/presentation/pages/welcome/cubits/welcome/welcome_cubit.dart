f4a120083b8385184dffce915555ece559f8b632
Yuriy Trofimov <yuriy.trofimov.ua@gmail.com>
2023-06-18 13:23:47 +0300

    Add the ability to log in from welcome page

---
+++ lib/presentation/pages/welcome/cubits/welcome/welcome_cubit.dart
@@ -0,0 +1,55 @@
+import 'package:bloc/bloc.dart';
+import 'package:debt_tracker/presentation/routing/app_router.dart';
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:freezed_annotation/freezed_annotation.dart';
+import 'package:injectable/injectable.dart';
+import 'package:google_sign_in/google_sign_in.dart';
+
+part 'welcome_state.dart';
+part 'welcome_cubit.freezed.dart';
+
+@Injectable()
+class WelcomeCubit extends Cubit<WelcomeState> {
+  WelcomeCubit(
+    this._firebaseAuth,
+    this._appRouter,
+  ) : super(const WelcomeState.data());
+
+  final AppRouter _appRouter;
+  final FirebaseAuth _firebaseAuth;
+
+  
+
+  void onContinueWithGooglePressed() {
+    state.mapOrNull(
+      data: (_) async {
+        emit(const WelcomeState.loading());
+        final googleSignIn = GoogleSignIn();
+        final crendational = await googleSignIn.signIn();
+        if (crendational != null) {
+          final googleAuth = await crendational.authentication;
+          final credential = GoogleAuthProvider.credential(
+            accessToken: googleAuth.accessToken,
+            idToken: googleAuth.idToken,
+          );
+          final result = await _firebaseAuth.signInWithCredential(credential);
+          if (result.user != null) {
+            _appRouter.replaceAll([const HomeRoute()]);
+          }
+        }
+      },
+    );
+  }
+
+  void onSignAsGuestPressed() {
+    state.mapOrNull(
+      data: (_) async {
+        emit(const WelcomeState.loading());
+        final result = await _firebaseAuth.signInAnonymously();
+        if (result.user != null) {
+          _appRouter.replaceAll([const HomeRoute()]);
+        }
+      },
+    );
+  }
+}
---
+++ lib/presentation/pages/welcome/cubits/welcome/welcome_state.dart
@@ -0,0 +1,7 @@
+part of 'welcome_cubit.dart';
+
+@freezed
+class WelcomeState with _$WelcomeState {
+  const factory WelcomeState.data() = _Data;
+  const factory WelcomeState.loading() = _Loading;
+}
