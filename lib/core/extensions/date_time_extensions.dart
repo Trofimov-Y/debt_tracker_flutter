5c0f6e1d4899e4e3dc009bc293ca56a9440fbf75
Yuriy Trofimov <yuriy.trofimov.ua@gmail.com>
2023-07-11 21:54:34 +0300

    Add new formatter to date time extensions

--- lib/core/extensions/date_time_extensions.dart
+++ lib/core/extensions/date_time_extensions.dart
@@ -3,6 +3,8 @@
 import 'package:intl/intl.dart';
 
 extension DateTimeExtensions on DateTime {
+  String get ddMMhhmm => DateFormat('dd MMMM hh:mm').format(this);
+
   String get dMMMMFormat => DateFormat('d MMMM').format(this);
 
   String get EEEddMMMYFormat => DateFormat('EEE, dd MMM, y').format(this);
