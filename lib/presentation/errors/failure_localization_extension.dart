8e461fe234524f4a97395f00def9a84c3243d4ea
Yuriy Trofimov <yuriy.trofimov.ua@gmail.com>
2023-07-17 16:15:18 +0300

    Add localization for failures

--- lib/presentation/errors/failure_localization_extension.dart
+++ lib/presentation/errors/failure_localization_extension.dart
@@ -9,46 +9,46 @@ extension PresentationFailureExtension on Failure {
         return S.of(context).googleSignInFailedFailure;
       },
       findGoogleAccount: (value) {
-        return S.of(context).googleSignInFailedFailure;
+        return S.of(context).googleSignInAccountNotFoundFailure;
       },
       signOut: (value) {
-        return S.of(context).googleSignInFailedFailure;
+        return S.of(context).signOutFailure;
       },
       getFeed: (value) {
-        return S.of(context).googleSignInFailedFailure;
+        return S.of(context).getFeedFailure;
       },
       getDebtsSummary: (value) {
-        return S.of(context).googleSignInFailedFailure;
+        return S.of(context).getDebtsSummaryFailure;
       },
       getDebts: (value) {
-        return S.of(context).googleSignInFailedFailure;
+        return S.of(context).getDebtsFailure;
       },
       getDebt: (value) {
-        return S.of(context).googleSignInFailedFailure;
+        return S.of(context).getDebtFailure;
       },
       createDebt: (value) {
-        return S.of(context).googleSignInFailedFailure;
+        return S.of(context).createDebtFailure;
       },
       deleteDebt: (value) {
-        return S.of(context).googleSignInFailedFailure;
+        return S.of(context).deleteDebtFailure;
       },
       editDebt: (value) {
-        return S.of(context).googleSignInFailedFailure;
+        return S.of(context).editDebtFailure;
       },
       deleteProfile: (value) {
-        return S.of(context).googleSignInFailedFailure;
+        return S.of(context).deleteProfileFailure;
       },
       deleteUserData: (value) {
-        return S.of(context).googleSignInFailedFailure;
+        return S.of(context).deleteUserDataFailure;
       },
       setSummaryCurrencyCode: (value) {
-        return S.of(context).googleSignInFailedFailure;
+        return S.of(context).setSummaryCurrencyCodeFailure;
       },
       setSummaryStatus: (value) {
-        return S.of(context).googleSignInFailedFailure;
+        return S.of(context).setSummaryStatusFailure;
       },
       getSummaryStatus: (value) {
-        return S.of(context).googleSignInFailedFailure;
+        return S.of(context).getSummaryStatusFailure;
       },
     );
   }
